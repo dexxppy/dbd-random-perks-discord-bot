35c47c3728d048159095b9113dd426e3a87fa56b
dexxppy <dex.a6310@gmail.com>
2025-08-11 21:25:22 +0200

    feat: data scraper from new website

--- data/item/addon_page_links.py
+++
@@ -1,7 +0,0 @@
-links = [
-    "https://deadbydaylight.fandom.com/wiki/Add-ons#Flashlights",
-    "https://deadbydaylight.fandom.com/wiki/Add-ons#Keys",
-    "https://deadbydaylight.fandom.com/wiki/Add-ons#Maps",
-    "https://deadbydaylight.fandom.com/wiki/Add-ons#Med-Kits",
-    "https://deadbydaylight.fandom.com/wiki/Add-ons#Toolboxes"
-]
